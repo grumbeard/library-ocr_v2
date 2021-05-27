1495f45dcb386a197af5628c6b0b8454df04b04e
Kaiying Fu <kaiying.fu@gmail.com>
2021-05-27 13:17:19 +0800

    Wire Routing for Books: Index, Show

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'books#index'
 
-  get '/books', to: 'books#index'
+  resources :books, only: [:index, :show]
 end
